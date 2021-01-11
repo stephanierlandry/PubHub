b1501f4f005ddbb1de26b6f95ce060caf0e74699
Stephanierlandry <stephanierlandry@gmail.com>
2021-01-10 21:09:56 -0600

    Tag DAO methods and queries written

--- src/examples/pubhub/utilities/DAOUtilities.java
+++ src/examples/pubhub/utilities/DAOUtilities.java
@@ -13,8 +13,8 @@ import examples.pubhub.dao.BookDAOImpl;
 public class DAOUtilities {
 
 	private static final String CONNECTION_USERNAME = "postgres";
-	private static final String CONNECTION_PASSWORD = "password";
-	private static final String URL = "jdbc:postgresql://localhost:5432/PubHub";
+	private static final String CONNECTION_PASSWORD = "P0stgr3s";
+	private static final String URL = "jdbc:postgresql://localhost:5433/PubHub";
 	private static Connection connection;
 	
 	public static synchronized Connection getConnection() throws SQLException {
